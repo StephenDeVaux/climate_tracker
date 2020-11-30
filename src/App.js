4d694ecb73f36763632ae3b20a69dccc8ec63b23
Stevey <60765528+StephenDeVaux@users.noreply.github.com>
2020-11-30 15:05:36 +1100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Climate Change Info website</div>
+    </div>
+  );
+}
+
+export default App;
