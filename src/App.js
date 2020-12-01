9daef991627dbacd078f5966f3bddf496e8fda33
Stevey <60765528+StephenDeVaux@users.noreply.github.com>
2020-12-01 22:04:44 +1100

    Integrated climate clock - looks shitty

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import VitalSignInfo from './VitalSignInfo';
 import Atlas from './Atlas';
 import { useContext } from 'react'
 import { InfoContext } from './InfoContext'
+import useScript from './useScript';
 
 function App() {
   const { year, co2, northIce, southIce, northIceNow, southIceNow, co2Now } = useContext(InfoContext)
-
+  // useScript("https://climateclock.world/widget-v1.js");
+  useScript("https://climateclock.world/widget-v2.js");
+  useScript("https://climateclock.world/flatten.js");
   return (
     <div className="App">
       <header className="App-header">
@@ -21,8 +24,8 @@ function App() {
           <div>Year: {year}</div>
           <div className="Atlas-co2" style={{ padding: `${co2 / 10}px` }}>
             <Atlas
-              southIce={southIce*10}
-              northIce={northIce*10}
+              southIce={southIce * 10}
+              northIce={northIce * 10}
             />
           </div>
         </div>
@@ -33,14 +36,14 @@ function App() {
           <div>Year: 2020 (Now)</div>
           <div className="Atlas-co2" style={{ padding: `${co2Now / 10}px` }}>
             <Atlas
-              southIce={southIceNow*10}
-              northIce={northIceNow*10}
+              southIce={southIceNow * 10}
+              northIce={northIceNow * 10}
             />
           </div>
         </div>
       </main>
-      <footer className="App-footer">Climte clock: 7 years 45days 2 hours 3min 5 seconds
-      
+      <footer className="App-footer">
+      <climate-clock />
       </footer>
     </div>
   );
